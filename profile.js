fa29f8d3d08e1de337f949a541975d38054b06d0
SamrithaSekar13 <samritha103@gmail.com>
2025-07-25 10:10:54 +0530

    Create profile.js

---
+++ profile.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function ProfilePage() {
+    return (
+        <div>
+            <h1>Profile Page</h1>
+            <p>Welcome to your profile! Here you can see your details and update your settings.</p>
+            {/* Add more profile-specific information or components here */}
+        </div>
+    );
+}
+
+export default ProfilePage;
